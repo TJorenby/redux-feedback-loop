d36134c867884622470703b44b9f21340ea9d0a2
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-09-26 17:38:27 -0500

    star-rating is capturing value from StarRating component and saving it in state on Question1.js

--- src/components/Question1/Question1.js
+++ src/components/Question1/Question1.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import './Question1.scss';
 import 'antd/dist/antd.css';
 import { Rate } from 'antd';
 import { Button } from 'antd';
+import StarRating from '../StarRating/StarRating';
+
+
 
 
 class Question1 extends Component {
+
+  state = {
+    rating: '',
+  }
+
+  setRating = (rating) => {
+    this.setState({
+      rating: rating
+    })
+  }
+
   render() {
+    console.log('rating is:', this.state.rating);
     return (
-     
-       <div className="card">
+
+      <div className="card">
 
         <div className="question">
           <h1>How Are You Feeling Today?</h1>
         </div>
 
         <div className="response-rating">
-          <Rate
-            defaultValue={2}
-            character={({ index }) => {
-              return index + 1;
-            }}
+
+          <StarRating
+            setRating={this.setRating}
           />
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
 
         <div className="next-btn">
