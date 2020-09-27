0cd9c6a1320b56f4e0577736e81511250566b29a
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-09-27 10:19:57 -0500

    star-rating for each question is now displaying for each question in ReviewFeedback component

--- src/components/ReviewFeedback/ReviewFeedback.js
+++ src/components/ReviewFeedback/ReviewFeedback.js
@@ -2,33 +2,82 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './ReviewFeedback.scss';
 import { connect } from 'react-redux';
-import { Divider } from 'antd';
+import { Divider, Button } from 'antd';
+import { FaStar } from 'react-icons/fa';
 
 class ReviewFeedback extends Component {
   render() {
     return (
-      <div>
-        <p>
-          How Are You Feeling Today?
-          {this.props.results.question1}
+      <div >
+        <div className="review-card">
 
-        </p>
-        <Divider />
-        <p>
-          How Well Are You Understanding The Content?
-          {this.props.results.question2}
-        </p>
-        <Divider />
-        <p>
-          How Well Are You Being Supported?
-          {this.props.results.question3}
-        </p>
-        <Divider />
-        <p>
-          Would You Like to Add A Comment?
-          {this.props.results.question4}
+          <div className="response-review">
+            <h3>How Are You Feeling Today?</h3>
+            <p>
+              {
+                [...Array(this.props.question1)].map(star => {
+                  return (
+                    <FaStar
+                      className="star"
+                      size={15}
+                      color={"#e4e5e9"}
+                    />
+                  )
+                })
+              }
+            </p>
 
-        </p>
+          </div>
+          <Divider />
+          <div className="response-review">
+            <h3>How well are you understanding the content?</h3>
+            <p>
+              {
+                [...Array(this.props.question2)].map(star => {
+                  return (
+                    <FaStar
+                      className="star"
+                      size={15}
+                      color={"#e4e5e9"}
+                    />
+                  )
+                })
+              }
+            </p>
+
+          </div>
+          <Divider />
+          <div className="response-review">
+            <h3>How well are you being supported?</h3>
+            <p>
+              {
+                [...Array(this.props.question3)].map(star => {
+                  return (
+                    <FaStar
+                      className="star"
+                      size={15}
+                      color={"#e4e5e9"}
+                    />
+                  )
+                })
+              }
+            </p>
+
+          </div>
+          <Divider />
+          <div className="comment-response-review">
+
+            <h3>Would You Like To Add A Comment?</h3>
+            <p className="comment-text">{this.props.question4} Test Comment with a lot of text in order to check layout. Test Comment with a lot of text in order to check layout. Test Comment with a lot of text in order to check layout. Test Comment with a lot of text in order to check layout. Test Comment with a lot of text in order to check layout. Test Comment with a lot of text in order to check layout</p>
+          </div>
+
+
+        </div>
+
+        <div className="submit-btn">
+          <Button>SUBMIT FEEDBACK</Button>
+        </div>
+
       </div>
     );
   }
@@ -37,7 +86,10 @@ class ReviewFeedback extends Component {
 const mapStateToProps = (reduxState) => {
   // Return props for the Location component
   return {
-    results: reduxState.resultsReducer
+    question1: reduxState.resultsReducer.question1,
+    question2: reduxState.resultsReducer.question2,
+    question3: reduxState.resultsReducer.question3,
+    question4: reduxState.resultsReducer.question4
   };
 }
 
